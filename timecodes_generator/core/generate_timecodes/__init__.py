e04f508afc248368df7ff9945a653c0f6af7f074
one-with-violets-in-her-lap <83793845+one-with-violets-in-her-lap@users.noreply.github.com>
2025-12-27 20:45:57 +0100

    fix missing export

--- timecodes_generator/core/generate_timecodes/__init__.py
+++ timecodes_generator/core/generate_timecodes/__init__.py
@@ -1,5 +1,6 @@
 from timecodes_generator.core.generate_timecodes.generate_timecodes import (
     generate_timecodes,
+    Timecode
 )
 
-__all__ = ["generate_timecodes"]
+__all__ = ["generate_timecodes", "Timecode"]
